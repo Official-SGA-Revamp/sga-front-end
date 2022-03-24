f0b36539598081aae47b12219364d4a39121b841
GabeArnell <43541642+GabeArnell@users.noreply.github.com>
2022-03-24 17:41:06 -0400

    Changed slideshow to video player
    Removed image switching with a background video player. Currently runs a placeholder video of my old blackjack assignment.

--- src/home/slideshow/slideshow.tsx
+++ src/home/slideshow/slideshow.tsx
@@ -1,44 +1,27 @@
 /**
- * slideshow.ts
+ * slideshow.tsx
  * 
- * TODO: Implement slideshow, here ya go Gabe!
- * 
- * Takes images from the public/images/slideshow and fades them in and out
+ * Displays looping video for front page
  * 
  */
  import {useState,useEffect} from 'react';
 
 export function SlideShowSection() {
-  const secondsPerSlide = 5;
-  const maxOpacity = 70;
-  
-  const imageList = [
-    "DesktopMCSGAFall2014.jpg",
-    "Marist SGA Background Dot.jpg",
-    "Marist SGA Background No Dot.jpg",
-    "Splash Logo.jpg"
-  ]
-
-  const [ index , setIndex] = useState<number>(0); 
-  const [ opacity , setOpacity] = useState<number>(maxOpacity); 
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      let fadeOut = setInterval(fadeout,10);
-
-      function fadeout(){
-
-      }
-      setIndex((index + 1)%imageList.length);
-    }, secondsPerSlide*1000);
-               // clearing interval
-    return () => clearInterval(timer);
-  });
-
+ 
   return (
     <div className="flex flex-row w-full h-2/3 justify-center">
       <div className="flex flex-col justify-end h-full w-2/3 bg-background-paper text-center text-text-primary items-center">
-        {<img src={"images/slideshow/"+imageList[index]} className={"opacity-"+opacity+" max-w-fit h-full z-70"} alt="slideshow"></img>}
+
+        {<video className={"max-w-fit h-full z-70"} 
+                    muted
+                    autoPlay={true}
+                    preload={'auto'}
+                    loop>
+
+          <source src="/videos/Proj2Demo.mp4" type="video/mp4"/>
+          Your browser does not support mp4 video types.
+
+        </video>}
         </div>
     </div>
   );
